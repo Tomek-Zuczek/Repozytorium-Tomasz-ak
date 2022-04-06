4964796d3d14146cdeb59e20d3fc716bd8249348
Tomasz Żak (Uczeń) <tzak166@technikumkreatywne.pl>
2022-04-06 10:50:02 +0200

    Updated readme

---
+++ podzielność.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+
+bool main2(int n)
+{
+   
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            if (n == 1)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n,l, p,lp;
+    
+
+    
+    cout << "Ile liczb pierwszych chcesz wygenerować: ";
+    cin >> n;
+
+
+    lp = 0;
+    p = 2;
+    bool isPrime;
+
+
+    while (lp != n)
+    {
+        isPrime = main2(lp);
+        if (isPrime) {
+            cout << lp<<endl;
+
+        }
+        lp++;
+    }
+
+    
+
+}
