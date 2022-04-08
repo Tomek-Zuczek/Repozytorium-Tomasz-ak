a34ddac70bc862430850bddf19a4cf4492eb1088
Tomasz Żak (Uczeń) <tzak166@technikumkreatywne.pl>
2022-04-08 09:35:10 +0200

    NWW

---
+++ nww nwd.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int nwd;
+
+    cout << "Podaj liczbę a: " << endl;
+    cin >> a;
+    cout << "Podaj liczbę b: " << endl;
+    cin >> b;
+    
+
+    while (a!=b) {
+
+        if (a < b) 
+        {
+            b -= a;
+        }
+
+        else 
+        {
+            a -= b;
+        }
+        
+    }
+    nwd = a;
+    cout << "Najwiekszy wspolny dzielnik: "<<nwd<< endl;
+
+
+
+    int c = a * b;
+    int nww;
+
+    nww = c / a;
+    cout << "Najwieksza wspolna wielokrotnosc: " << nww << endl;
+
+    
+
+
+    
+}
+
